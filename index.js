1c522332fdd03cc349851fa5f28c4ee2ed48ac09
Dkdkdkdk <ahmetmerk779@gmail.com>
2026-06-22 18:28:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.get('/', (req, res) => {
+    res.send('Minecraft Bot Sunucusu Aktif!');
+});
+
+app.listen(port, () => {
+    console.log(`Sunucu http://localhost:${port} adresinde çalışıyor.`);
+});
