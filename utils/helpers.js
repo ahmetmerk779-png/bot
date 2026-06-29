30bfaaf7154cace8314723b3cbb9c9537a4a9d52
Dkdkdkdk <ahmetmerk779@gmail.com>
2026-06-29 13:45:31 +0300

    Update helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -18,9 +18,21 @@ function distance(pos1, pos2) {
   );
 }
 
+function formatCoords(x, y, z) {
+  return `${Math.round(x)}, ${Math.round(y)}, ${Math.round(z)}`;
+}
+
+function getDirection(angle) {
+  const directions = ['kuzey', 'kuzeydoğu', 'doğu', 'güneydoğu', 'güney', 'güneybatı', 'batı', 'kuzeybatı'];
+  const index = Math.round(((angle % (2 * Math.PI)) / (2 * Math.PI)) * 8) % 8;
+  return directions[index];
+}
+
 module.exports = {
   sleep,
   randomInt,
   randomFloat,
-  distance
+  distance,
+  formatCoords,
+  getDirection
 };
