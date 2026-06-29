66a27e099c9bc9125cc76c3cc566153c6776a58f
Dkdkdkdk <ahmetmerk779@gmail.com>
2026-06-29 13:23:57 +0300

    Update index.js

--- skills/index.js
+++ skills/index.js
@@ -6,7 +6,7 @@ const useCraftingTable = require('./useCraftingTable');
 const observe = require('./observe');
 const explore = require('./explore');
 const waypoint = require('./waypoint');
-const shareLocation = require('./shareLocation');   // YENİ
+const coords = require('./coords');   // YENİ
 const plan = require('./plan');
 
 module.exports = {
@@ -18,6 +18,6 @@ module.exports = {
   observe,
   explore,
   waypoint,
-  shareLocation,    // YENİ
+  coords,    // YENİ
   plan
 };
