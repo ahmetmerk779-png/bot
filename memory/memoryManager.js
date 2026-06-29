bbaead651740b1bd14643f06d118a8c15161077f
Dkdkdkdk <ahmetmerk779@gmail.com>
2026-06-29 13:13:33 +0300

    Update memoryManager.js

--- memory/memoryManager.js
+++ memory/memoryManager.js
@@ -1,51 +1,68 @@
 const fs = require('fs');
 const config = require('../config');
 
-// Mevcut fonksiyonlar (loadMemory, saveMemory, addEvent, addKnowledge) zaten var
+// Mevcut fonksiyonlar (loadMemory, saveMemory, addEvent, addKnowledge, loadDiscoveries, saveDiscoveries, addDiscovery, getDiscoveries, clearDiscoveries) zaten var
 // Bunların altına ekle:
 
-function loadDiscoveries() {
+function loadWaypoints() {
   try {
-    const data = fs.readFileSync('./memory/discoveries.json', 'utf8');
+    const data = fs.readFileSync('./memory/waypoints.json', 'utf8');
     return JSON.parse(data);
   } catch {
-    return { discoveries: [] };
+    return { waypoints: [] };
   }
 }
 
-function saveDiscoveries(discoveriesData) {
-  fs.writeFileSync('./memory/discoveries.json', JSON.stringify(discoveriesData, null, 2));
+function saveWaypoints(waypointsData) {
+  fs.writeFileSync('./memory/waypoints.json', JSON.stringify(waypointsData, null, 2));
 }
 
-function addDiscovery(type, coords, name) {
-  const data = loadDiscoveries();
-  data.discoveries.push({
-    type: type,
-    coords: coords,
-    name: name || type,
-    timestamp: Date.now()
-  });
-  saveDiscoveries(data);
-  return data.discoveries;
+function addWaypoint(name, coords) {
+  const data = loadWaypoints();
+  // Aynı isimde waypoint varsa güncelle
+  const existingIndex = data.waypoints.findIndex(w => w.name === name);
+  if (existingIndex !== -1) {
+    data.waypoints[existingIndex].coords = coords;
+    data.waypoints[existingIndex].timestamp = Date.now();
+  } else {
+    data.waypoints.push({
+      name: name,
+      coords: coords,
+      timestamp: Date.now()
+    });
+  }
+  saveWaypoints(data);
+  return data.waypoints;
 }
 
-function getDiscoveries() {
-  const data = loadDiscoveries();
-  return data.discoveries;
+function getWaypoint(name) {
+  const data = loadWaypoints();
+  return data.waypoints.find(w => w.name === name) || null;
 }
 
-function clearDiscoveries() {
-  saveDiscoveries({ discoveries: [] });
+function getWaypoints() {
+  const data = loadWaypoints();
+  return data.waypoints;
+}
+
+function deleteWaypoint(name) {
+  const data = loadWaypoints();
+  data.waypoints = data.waypoints.filter(w => w.name !== name);
+  saveWaypoints(data);
+  return data.waypoints;
+}
+
+function clearWaypoints() {
+  saveWaypoints({ waypoints: [] });
 }
 
 module.exports = {
-  loadMemory,
-  saveMemory,
-  addEvent,
-  addKnowledge,
-  loadDiscoveries,
-  saveDiscoveries,
-  addDiscovery,
-  getDiscoveries,
-  clearDiscoveries
+  // ... mevcut fonksiyonlar
+  loadWaypoints,
+  saveWaypoints,
+  addWaypoint,
+  getWaypoint,
+  getWaypoints,
+  deleteWaypoint,
+  clearWaypoints
 };
