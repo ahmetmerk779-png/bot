6ae59249766f40e3d68df1c2a22e64c48cb83c6d
Dkdkdkdk <ahmetmerk779@gmail.com>
2026-06-29 20:11:56 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const path = require('path');
+
 const CONFIG_PATH = path.join(__dirname, 'config.json');
 
 const defaults = {
-  botName: 'AFK_Bot',
+  botName: 'BenimAIBot',
   serverHost: 'aesirmc.com',
   serverPort: 25565,
   version: '1.8.9',
@@ -16,13 +17,12 @@ function loadConfig() {
     if (fs.existsSync(CONFIG_PATH)) {
       return { ...defaults, ...JSON.parse(fs.readFileSync(CONFIG_PATH, 'utf8')) };
     }
-  } catch (e) {}
+  } catch {}
   return { ...defaults };
 }
 
 function saveConfig(data) {
   fs.writeFileSync(CONFIG_PATH, JSON.stringify(data, null, 2));
-  return true;
 }
 
 module.exports = { loadConfig, saveConfig };
